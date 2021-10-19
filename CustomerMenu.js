0eebb8ae7d921c7796c48d5cc77d6fe32da6e5ea
Manthi Nimaya Perera <54572453+NimayaPerera@users.noreply.github.com>
2021-10-19 22:34:33 +0530

    customer menu

---
+++ CustomerMenu.js
@@ -0,0 +1,19 @@
+export const CustomerMenu = [
+    {
+      title: 'PencilArts',
+      path: '/customer_pencilarts',
+      cName: 'dropdown-link'
+    },
+   
+    {
+      title: 'Exhibitions',
+      path: '/customer_exhibitions',
+      cName: 'dropdown-link'
+    },
+    {
+      title: 'About Artists',
+      path: '/artists',
+      cName: 'dropdown-link'
+    }
+   
+  ];
