89a73c6e9cbe11d8cbf473b07fbda80330a98b65
Manthi Nimaya Perera <54572453+NimayaPerera@users.noreply.github.com>
2021-10-19 22:30:20 +0530

    admin drop down

---
+++ AdminDropdown.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { AdminMenuItems } from './AdminMenuItems';
+import './Dropdown.css';
+import { Link } from 'react-router-dom';
+
+function AdminDropdown() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  return (
+    <>
+      <ul
+        onClick={handleClick}
+        className={click ? 'dropdown-menu clicked' : 'dropdown-menu'}
+      >
+        {AdminMenuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              <Link
+                className={item.cName}
+                to={item.path}
+                onClick={() => setClick(false)}
+              >
+                {item.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default AdminDropdown;
